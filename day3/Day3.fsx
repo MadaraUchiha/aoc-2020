1957a37e32ec91eb54dd784f183cc7f520c71cb9
Madara Uchiha <me@madara.ninja>
2020-12-04 00:45:08 +0200

    Refactoring and cleanup

--- day3/Day3.fsx
+++ day3/Day3.fsx
@@ -1,36 +1,45 @@
 #load "Day3Input.fsx"
 open Day3Input
 
-type Tile =
-    | Tree
-    | Snow
+// Utility
+module Array =
+    let filteri predicate values =
+        values
+        |> Array.indexed
+        |> Array.filter (fun (i, value) -> predicate i value)
+        |> Array.map snd
 
-let tileFromChar c =
-    match c with
-    | '#' -> Tree
-    | _ -> Snow
+module Tile =
+    type Tile =
+        | Tree
+        | Snow
 
-let rowFromString s = Array.ofSeq s |> Array.map tileFromChar
+    let private tileFromChar c =
+        match c with
+        | '#' -> Tree
+        | _ -> Snow
+
+    let rowFromString s = Array.ofSeq s |> Array.map tileFromChar
 
 let parseInput (input: string) =
     input.Split([| '\n' |])
-    |> Array.map ((fun r -> r.Trim()) >> rowFromString)
+    |> Array.map ((fun r -> r.Trim()) >> Tile.rowFromString)
 
 type Vector = { x: int; y: int }
 
 let tileAtIndex<'T> (row: 'T []) index = row.[index % Array.length row]
 
-let solveFor (tiles: Tile [] []) { x = advanceX; y = advanceY } =
+
+let solveFor { x = advanceX; y = advanceY } tiles =
     tiles
-    |> Array.indexed
-    |> Array.filter (fun (i, _) -> i % advanceY = 0)
-    |> Array.mapi (fun i (_, row) -> tileAtIndex row (i * advanceX))
-    |> Array.filter (fun t -> t = Tree)
+    |> Array.filteri (fun i _ -> i % advanceY = 0)
+    |> Array.mapi (fun i row -> tileAtIndex row (i * advanceX))
+    |> Array.filter (fun t -> t = Tile.Tree)
     |> Array.length
 
 let tiles = parseInput day3Input
 
-let day3Part1Solution = solveFor tiles { x = 3; y = 1 }
+let day3Part1Solution = tiles |> solveFor { x = 3; y = 1 }
 
 let day3Part2Solution =
     let slopes =
@@ -41,7 +50,7 @@ let day3Part2Solution =
           { x = 1; y = 2 } ]
 
     slopes
-    |> List.map ((fun slope -> solveFor tiles slope) >> int64)
+    |> List.map ((fun slope -> solveFor slope tiles) >> int64)
     |> List.fold (*) (int64 1)
 
 printfn "Day 3 part 1 solution: %i" day3Part1Solution
