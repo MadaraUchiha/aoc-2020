736510d30b54a08d51f4b2595c15f462b35695fc
Madara Uchiha <me@madara.ninja>
2020-12-04 18:23:17 +0200

    Day 4

---
+++ day4/Day4.fsx
@@ -0,0 +1,158 @@
+open System
+open System.Text.RegularExpressions
+
+#load "Day4Input.fsx"
+open Day4Input
+
+module Year =
+    type T = Year of int
+
+
+    let valid from thru (Year year) = year >= from && year <= thru
+
+    let create year = Year year
+
+module Height =
+    type T = Height of (int * string)
+
+    let create (height: string) =
+        let numeric =
+            if (height.Length > 2) then height.[..(height.Length - 3)] else "0"
+
+        let unit =
+            height.[(height.Length - 2)..height.Length]
+
+        Height(int numeric, unit)
+
+
+    let valid (Height height) =
+        let (height, units) = height
+
+
+        match units with
+        | "cm" -> height >= 150 && height <= 193
+        | "in" -> height >= 59 && height <= 76
+        | _ -> false
+
+module Color =
+    type T = Color of string
+    let create color = Color color
+
+    let valid (Color color) = Regex.IsMatch(color, @"^#[0-9a-f]{6}$")
+
+module EyeColor =
+    type T = EyeColor of string
+    let create color = EyeColor color
+
+    let valid (EyeColor color) =
+        match color with
+        | "amb"
+        | "blu"
+        | "brn"
+        | "gry"
+        | "grn"
+        | "hzl"
+        | "oth" -> true
+        | _ -> false
+
+module PassportId =
+    type T = PassportId of string
+    let create pid = PassportId pid
+
+    let valid (PassportId pid) = Regex.IsMatch(pid, @"^\d{9}$")
+
+type Passport =
+    { byr: Year.T option
+      iyr: Year.T option
+      eyr: Year.T option
+      hgt: Height.T option
+      hcl: Color.T option
+      ecl: EyeColor.T option
+      pid: PassportId.T option
+      cid: string option }
+
+
+let emptyPassport =
+    { byr = None
+      iyr = None
+      eyr = None
+      hgt = None
+      hcl = None
+      ecl = None
+      pid = None
+      cid = None }
+
+let isPassportPart1Valid { byr = byr; iyr = iyr; eyr = eyr; hgt = hgt; hcl = hcl; ecl = ecl; pid = pid } =
+    Option.isSome byr
+    && Option.isSome iyr
+    && Option.isSome eyr
+    && Option.isSome hgt
+    && Option.isSome hcl
+    && Option.isSome ecl
+    && Option.isSome pid
+
+let isPassportPart2Valid passport =
+    if (not (isPassportPart1Valid passport)) then
+        false
+    else
+        let { byr = byr; iyr = iyr; eyr = eyr; hgt = hgt; hcl = hcl; ecl = ecl; pid = pid } = passport
+
+        Year.valid 1920 2002 (Option.get byr)
+        && Year.valid 2010 2020 (Option.get iyr)
+        && Year.valid 2020 2030 (Option.get eyr)
+        && Height.valid (Option.get hgt)
+        && Color.valid (Option.get hcl)
+        && EyeColor.valid (Option.get ecl)
+        && PassportId.valid (Option.get pid)
+
+let passportEntryOfString passport (str: string) =
+    let keyValue = str.Trim().Split(':')
+    match keyValue with
+    | [| key; value |] ->
+        match key with
+        | "byr" ->
+            { passport with
+                  byr = Some(Year.create (int value)) }
+        | "iyr" ->
+            { passport with
+                  iyr = Some(Year.create (int value)) }
+        | "eyr" ->
+            { passport with
+                  eyr = Some(Year.create (int value)) }
+        | "hgt" ->
+            { passport with
+                  hgt = Some(Height.create value) }
+        | "hcl" ->
+            { passport with
+                  hcl = Some(Color.create value) }
+        | "ecl" ->
+            { passport with
+                  ecl = Some(EyeColor.create value) }
+        | "pid" ->
+            { passport with
+                  pid = Some(PassportId.create value) }
+        | "cid" -> { passport with cid = Some value }
+        | key -> failwith (sprintf "Invalid key: %s." key)
+    | _ -> failwith (sprintf "Invalid input. Line: %s" str)
+
+let passportOfString (str: string) =
+    str.Trim().Split([| '\n'; ' ' |], StringSplitOptions.None)
+    |> Array.fold passportEntryOfString emptyPassport
+
+let parseInput (input: string) =
+    input.Trim().Split([| "\n\n" |], StringSplitOptions.None)
+    |> List.ofArray
+    |> List.map passportOfString
+
+let day4Part1Solution =
+    parseInput day4Input
+    |> List.filter isPassportPart1Valid
+    |> List.length
+
+let day4Part2Solution =
+    parseInput day4Input
+    |> List.filter isPassportPart2Valid
+    |> List.length
+
+printfn "Day 4 part 1 solution: %i" day4Part1Solution
+printfn "Day 4 part 2 solution: %i" day4Part2Solution
