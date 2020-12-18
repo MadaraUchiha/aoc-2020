4397ef2d6ba2e715e3b7ad388bbebd9d18b5a53d
Madara Uchiha <me@madara.ninja>
2020-12-18 15:34:33 +0200

    Day 18

---
+++ day18/Day18.fsx
@@ -0,0 +1,102 @@
+#load "../common/Utils.fsx"
+#load "Day18Input.fsx"
+
+open Utils
+open Day18Input
+
+type Value =
+    | Number of int
+    | Brackets of string
+
+type Operator =
+    | Mul
+    | Add
+
+type Expression = Operator * Value
+
+type ParserState =
+    { Braces: int
+      Result: Expression list
+      FormingExpression: string
+      CurrentOperator: Operator }
+
+let x =
+    [ Add, Number 1
+      Mul, Number 3
+      Add, Brackets "3 + 4" ]
+
+let parseExpression (exp: string) =
+    let folder acc (c: char) =
+        let sc = string c
+        match acc.Braces, sc with
+        | n, _ when n < 0 -> failwith "Imbalanced brackets."
+        | 1, ")" ->
+            { acc with
+                  Braces = 0
+                  Result =
+                      acc.Result
+                      @ [ (acc.CurrentOperator, Brackets(acc.FormingExpression)) ]
+                  FormingExpression = "" }
+        | n, "(" when n > 0 ->
+            { acc with
+                  FormingExpression = acc.FormingExpression + sc
+                  Braces = acc.Braces + 1 }
+        | n, ")" when n > 0 ->
+            { acc with
+                  FormingExpression = acc.FormingExpression + sc
+                  Braces = acc.Braces - 1 }
+        | n, c when n > 0 ->
+            { acc with
+                  FormingExpression = acc.FormingExpression + sc }
+        | _, Scan "%i" i ->
+            { acc with
+                  Result = acc.Result @ [ (acc.CurrentOperator, Number i) ] }
+        | _, "+" -> { acc with CurrentOperator = Add }
+        | _, "*" -> { acc with CurrentOperator = Mul }
+        | _, "(" -> { acc with Braces = acc.Braces + 1 }
+        | _, ")" -> { acc with Braces = acc.Braces - 1 }
+        | _, " " -> acc
+        | _, other -> failwithf "Invalid expression character %s" other
+
+    let result =
+        exp
+        |> Seq.fold
+            (folder)
+               { Braces = 0
+                 Result = List.empty
+                 FormingExpression = ""
+                 CurrentOperator = Add }
+
+    result.Result
+
+
+let rec solve expressions =
+    expressions
+    |> Seq.fold (fun acc exp ->
+        match exp with
+        | Add, Number n -> acc + (int64 n)
+        | Add, Brackets s -> acc + (solve (parseExpression s))
+        | Mul, Number n -> acc * (int64 n)
+        | Mul, Brackets s -> acc * (solve (parseExpression s))) 0L
+
+let parseInput input =
+    input |> split "\n" |> Seq.map parseExpression
+
+let part2ify =
+    (RegExp.replace @"\(" "(("
+     >> RegExp.replace @"\)" "))"
+     >> RegExp.replace @"\*" ") * (")
+    >> (fun s -> "( " + s + " )")
+
+let parseInputPart2 input =
+    input
+    |> split "\n"
+    |> Seq.map (part2ify >> parseExpression)
+
+let day18Part1Solution = parseInput day18Input |> Seq.sumBy solve
+
+let day18Part2Solution =
+    parseInputPart2 day18Input |> Seq.sumBy solve
+
+printfn "Day 18 part 1 solution: %i" day18Part1Solution
+printfn "Day 18 part 2 solution: %i" day18Part2Solution
